da23986fad4789896dfaeb0c7a76bd0f87425a43
Hector Norza <hnorza@Hectors-MBP.officenomads.com>
2018-07-13 15:44:37 -0700

    added working app

---
+++ albums/index.js
@@ -0,0 +1,16 @@
+//import a library to help create component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+//Create component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+    </View>
+  );
+
+//Render it to device
+AppRegistry.registerComponent('albums', () => App);
